b588099d8952ef0443555d98f8dad5d00e15e16b
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-10-02 10:01:53 +0200

    Comentario profe para como hacer LOG

--- TO WORK/EXP_Engine_Sol/log.cpp
+++ TO WORK/EXP_Engine_Sol/log.cpp
@@ -13,4 +13,5 @@ void log(const char file[], int line, const char* format, ...)
 	va_end(ap);
 	sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
 	OutputDebugString(tmp_string2);
+	//sI TENGO ACCESO APP Y EDITOR Y MANDARLE LA TMP STRING AL LOG DEL EDITOR
 }

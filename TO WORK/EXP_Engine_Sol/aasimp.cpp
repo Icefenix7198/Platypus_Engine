02da29f93eb1a8ffd85bc5f9debfd8e10eae307d
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-10-13 23:04:45 +0200

    Load meshes WIP

--- TO WORK/EXP_Engine_Sol/aasimp.cpp
+++ TO WORK/EXP_Engine_Sol/aasimp.cpp
@@ -2,12 +2,23 @@
 
 void aasimp::Load(const char* file_path)
 {
+	Mesh ourMesh;
 	const aiScene* scene = aiImportFile(file_path, aiProcessPreset_TargetRealtime_MaxQuality);
-	if (scene != nullptr && aiScene > HasMeshes())
+	if (scene != nullptr && scene-> HasMeshes())
 	{
 		// Use scene->mNumMeshes to iterate on scene->mMeshes array
+
+		
+		ourMesh.num_vertex /*= aiMesh->mNumVertices*/;
+		ourMesh.vertex = new float[ourMesh.num_vertex * 3];
+		memcpy(ourMesh.vertex, aiMesh->mVertices, sizeof(float) * ourMesh.num_vertices * 3);
+		LOG("New mesh with %d vertices", m.num_vertices);
+
 		aiReleaseImport(scene);
 	}
 	else
-		LOG(“Error loading scene % s”, file_path);
+	{
+		LOG("Error loading scene % s", file_path);
+	}
+		
 }
---
+++ TO WORK/EXP_Engine_Sol/aasimp.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Assimp/include/cimport.h"
+#include "Assimp/include/scene.h"
+#include "Assimp/include/postprocess.h"
+#include "Assimp/include/mesh.h"
+
+#include "Globals.h"
+
+#pragma comment (lib, "Assimp/libx86/assimp.lib")
+
+struct Mesh 
+{
+	uint id_index = 0; // index in VRAM
+	uint num_index = 0;
+	uint* index = nullptr;
+	uint id_vertex = 0; // unique vertex in VRAM
+	uint num_vertex = 0;
+	float* vertex = nullptr;
+};
+
+namespace aasimp
+{
+	void Load(const char* file_path);
+}

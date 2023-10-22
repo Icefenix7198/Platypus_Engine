ed17deac7ee85bbf95d8e781c8ad83825b127219
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-10-22 22:45:15 +0200

    Variables y constructor transform

--- TO WORK/EXP_Engine_Sol/ComponentTransform.cpp
+++ TO WORK/EXP_Engine_Sol/ComponentTransform.cpp
@@ -1,11 +1,60 @@
 #include "ComponentTransform.h"
 
+#include "MathGeoLib/include/Math/Quat.h"
 
 ComponentTransform::ComponentTransform()
 {
+	translation.x = 0;
+	translation.y = 0;
+	translation.z = 0;
+	pos = {translation.x,translation.y,translation.z};
+
+	scaling.x = 0;
+	scaling.y = 0;
+	scaling.z = 0;
+	scale = { scaling.x, scaling.y, scaling.z };
+
+	rotation.x = 0;
+	rotation.y = 0;
+	rotation.z = 0;
+	rotation.w = 0;
+	rot = {rotation.x, rotation.y, rotation.z, rotation.w};
+}
+
+ComponentTransform::ComponentTransform(aiVector3D vecPos)
+{
+	translation.x = vecPos.x;
+	translation.y = vecPos.y;
+	translation.z = vecPos.z;
+	pos = { translation.x,translation.y,translation.z };
+
+	scaling.x = 0;
+	scaling.y = 0;
+	scaling.z = 0;
+	scale = { scaling.x, scaling.y, scaling.z };
+
+	rotation.x = 0;
+	rotation.y = 0;
+	rotation.z = 0;
+	rotation.w = 0;
+	rot = { rotation.x, rotation.y, rotation.z, rotation.w };
+}
+
+ComponentTransform::ComponentTransform(aiVector3D vecPos, aiVector3D vecScale, aiQuaternion quatRot)
+{
+	translation = vecPos;
+	pos = { translation.x,translation.y,translation.z };
+
+	scaling = vecScale;
+	scale = { scaling.x, scaling.y, scaling.z };
+
+	rotation = quatRot;
+	rot = { rotation.x, rotation.y, rotation.z, rotation.w };
 }
 
 ComponentTransform::~ComponentTransform()
 {
 }
+
+
 
--- TO WORK/EXP_Engine_Sol/ComponentTransform.h
+++ TO WORK/EXP_Engine_Sol/ComponentTransform.h
@@ -4,6 +4,11 @@
 #include "Globals.h"
 #include "GameObject.h"
 #include "Component.h"
+#include "MathGeoLib/include/Math/float3.h"
+#include "aasimp.h"
+#include "Assimp/include/quaternion.h"
+#include "Assimp/include/vector3.h"
+#include "MathGeoLib/include/Math/Quat.h"
 
 #include <vector>
 #include <string>
@@ -13,7 +18,20 @@ class ComponentTransform : public Component
 {
 public:
 	ComponentTransform();
+	ComponentTransform(aiVector3D vecPos); //Only position
+	ComponentTransform(aiVector3D vecPos, aiVector3D vecScale, aiQuaternion quatRot); //All parameters
+
+
 	~ComponentTransform();
+
+	aiVector3D translation, scaling;
+	aiQuaternion rotation;
+	
+	//node->mTransformation.Decompose(scaling, rotation, translation); //Esto es conseguir es scenes creo?
+	
+	float3 pos;
+	float3 scale;
+	math::Quat rot;
 
 private:
 

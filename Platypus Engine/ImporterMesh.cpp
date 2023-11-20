f4661c2d31a77871f19de59283b7705b113080a3
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-11-20 10:03:12 +0100

    Importer meshes creados
    Sin codigos

---
+++ Platypus Engine/ImporterMesh.cpp
@@ -0,0 +1,21 @@
+#include "ImporterMesh.h"
+#include "Globals.h"
+
+#include "SDL\include\SDL.h"
+#include "PhysFS\include\physfs.h" //Works better than direct.h
+
+#pragma comment (lib, "PhysFS/libx86/physfs.lib")
+
+void ImporterMesh::Import()
+{
+	
+}
+
+__int64 ImporterMesh::Save()
+{
+	return 0;
+}
+
+void ImporterMesh::Load()
+{
+}

309d84819e772acd7c17d407c9a9f27a8fa811e2
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-12-02 22:15:12 +0100

    Import mesh

--- Platypus Engine/ImporterMesh.h
+++ Platypus Engine/ImporterMesh.h
@@ -4,14 +4,22 @@
 #include "Importer.h"
 #include "ResourceMesh.h"
 
+#include "Assimp/include/cimport.h"
+#include "Assimp/include/scene.h"
+#include "Assimp/include/postprocess.h"
+#include "Assimp/include/mesh.h"
+#include "Glew/include/glew.h"
+
+#include "MathGeoLib/include/MathGeoLib.h"
+
 class ImporterMesh : public Importer
 {
 public:
 
 	//Transform/copy values from asset file to our structure of info
-	virtual void Import();
+	virtual void Import(aiScene* scene, ResourceMesh& reMesh);
 	//Save from our struct of info to library
-	uint64_t Save(ResourceMesh rMesh); //Todo: Preguntar al profe si eltipo de la funcion esta bien o que 
+	uint64_t Save(ResourceMesh rMesh, char** buffer); //Todo: Preguntar al profe si eltipo de la funcion esta bien o que 
 	//Load  info from library to our struct	
 	virtual void Load(ResourceMesh &rMesh, char* buffer);
 
